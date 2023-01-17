1bddd45fef2f0b6e4721e372373e0bb30d201b7d
Rafeef <rafeefalmahy@gmail.com>
2023-01-17 22:51:14 +0200

    UI design
    login
    signUp
    spalsh

--- app/src/main/java/com/example/foodplanner/view/fragment/LoginFragment.java
+++ app/src/main/java/com/example/foodplanner/view/fragment/LoginFragment.java
@@ -26,13 +26,15 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginFragment extends Fragment implements LogInFragmentInterface {
-    Button signInbtn, logingoogle;
+    Button signInbtn;
+    FloatingActionButton logingoogle;
     EditText loginEmail, loginPassword;
     TextView signUp;
     LogInFragmentPresenter logInFragmentPresenter;
