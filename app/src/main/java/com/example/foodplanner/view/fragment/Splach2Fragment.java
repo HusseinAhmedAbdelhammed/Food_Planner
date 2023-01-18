429b243d90761e1d1c115bf28d855f411288bcad
Rafeef <rafeefalmahy@gmail.com>
2023-01-18 12:33:56 +0200

    splachScreen2

--- app/src/main/java/com/example/foodplanner/view/fragment/Splach2Fragment.java
+++ app/src/main/java/com/example/foodplanner/view/fragment/Splach2Fragment.java
@@ -15,41 +15,36 @@ import android.widget.TextView;
 import com.example.foodplanner.R;
 import com.example.foodplanner.utils.NavigatorClass;
 
-public class Splach1Fragment extends Fragment {
+public class Splach2Fragment extends Fragment {
+
     Button next;
     TextView skip;
-
-
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
-        return inflater.inflate(R.layout.fragment_splach1, container, false);
+        return inflater.inflate(R.layout.fragment_splach2, container, false);
     }
-
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        next = view.findViewById(R.id.spalsh1Nextbtn);
-        skip = view.findViewById(R.id.spalsh1SkipTV);
+        next = view.findViewById(R.id.spalsh2Nextbtn);
+        skip = view.findViewById(R.id.spalsh2SkipTV);
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavigatorClass.navigateBetweenFragments(view, R.id.action_splach1Fragment_to_splach2Fragment);
+                NavigatorClass.navigateBetweenFragments(view, R.id.action_splach2Fragment_to_signupFragment);
             }
         });
         skip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavigatorClass.navigateBetweenFragments(view, R.id.action_splach1Fragment_to_signupFragment);
+                NavigatorClass.navigateBetweenFragments(view, R.id.action_splach2Fragment_to_signupFragment);
 
             }
         });
